4b32aa6a05b2eb4febd06af03452d0dfa309deef
Guilherme Girão <guigirao@hotmail.com>
2021-10-13 11:01:23 -0300

    fix(package): update versions of next, react and swr

--- src/hooks/useInfiniteFetch.js
+++ src/hooks/useInfiniteFetch.js
@@ -1,4 +1,4 @@
-import { useSWRInfinite } from 'swr';
+import useSWRInfinite from 'swr/infinite';
 import { api } from '../services/api';
 
 const getKey = (pageIndex, previousPageData, url, pageSize) => {
